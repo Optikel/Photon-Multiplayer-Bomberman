427ea8f73ef489103e0d4ee5d78ed89e565d54c0
jeryl lah <56944877+OpticalLuck@users.noreply.github.com>
2021-11-15 18:07:49 +0800

    Woah i can movement

---
+++ Assets/Scripts/BombSpawn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombSpawn : MonoBehaviour
+{
+    [Range(1f, 10f)]
+    public float Distance = 1f;
+    public GameObject BombPrefab;
+    public CharacterMovement movementScript;
+
+    private Transform characterTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Distance = Mathf.Clamp(Distance, 1f, 5f);
+        //GameObject obj;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 dirVector = Quaternion.Euler(0, movementScript.targetAngle, 0) * Vector3.forward;
+
+            GameObject bomb = Instantiate(BombPrefab, characterTransform.localPosition + dirVector * Distance, Quaternion.identity) as GameObject;
+        }
+
+        
+
+    }
+}
--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -3,29 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
-    public CharacterController controller;
+    //public CharacterController controller;
     public float movementSpeed = 1f;
+    public float turnSmoothness = 0.1f;
+
+    [HideInInspector]
+    public float targetAngle;
+    
+    CharacterController controller;
+    float turnSmoothVelocity;
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = new Vector3(0,0,0);
+        Vector3 direction = Vector3.zero;
         if (Input.GetKey(KeyCode.W))
-            direction = Vector3.forward;
+            direction += Vector3.forward;
         if (Input.GetKey(KeyCode.S))
-            direction = Vector3.back;
+            direction += Vector3.back;
         if (Input.GetKey(KeyCode.A))
-            direction = Vector3.left;
+            direction += Vector3.left;
         if (Input.GetKey(KeyCode.D))
-            direction = Vector3.right;
+            direction += Vector3.right;
 
-        controller.Move(direction * movementSpeed * 0.1f);
-        if(direction.magnitude > 0)
-            GetComponent<Transform>().localRotation = Quaternion.LookRotation(direction);
+        
+
+        if (direction.magnitude >= 0.1f)
+        {
+            direction.Normalize();
+            targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            controller.Move(direction * movementSpeed * Time.deltaTime);
+        }
+
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothness);
+        transform.rotation = Quaternion.Euler(0, angle, 0);
     }
 }
