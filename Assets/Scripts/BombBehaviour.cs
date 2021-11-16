178f650e1d22eb7d7d16a39fbb5e9aca6d0c864c
jeryl lah <56944877+OpticalLuck@users.noreply.github.com>
2021-11-17 03:58:50 +0800

    Bomb Almost Done
    Spawning done
    Deletion Done
    TODO: Add explosion

---
+++ Assets/Scripts/BombBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombBehaviour : MonoBehaviour
+{
+    public GameObject BombMesh;
+    public GameObject Explosion;
+    public int Power = 3; //Distance from origin, meaning power 3 is 2 + 1 + 3 = 5 tiles
+    public float Timer = 3f;
+    public float RateOfBlink = 1f;
+
+    private float CountDown = 0f;
+    private float BlinkingTimer = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CountDown += Time.deltaTime;
+
+        //Hide bomb Mesh
+        if(CountDown >= Timer)
+        {
+            BombMesh.SetActive(false);
+        }
+
+        //Destroy Object
+        if (CountDown >= Timer + 1)
+        {
+            Debug.Log("COuntdown:" + CountDown + "| Timer: " + Timer + 1);
+            Object.Destroy(BombMesh.GetComponentInParent<Transform>().gameObject);
+        }
+
+        Blink(RateOfBlink, Color.black, Color.red);
+    }
+
+    void Blink(float intervals, Color originalColor, Color blinkColor)
+    {
+        BlinkingTimer += Time.deltaTime;
+
+        Material mat = BombMesh.GetComponent<MeshRenderer>().material;
+        if(BlinkingTimer > RateOfBlink * 0.5f)
+        {
+            mat.SetColor("_Color", mat.GetColor("_Color") == originalColor ? blinkColor : originalColor);
+            BlinkingTimer = 0;
+        }
+    }
+}
--- Assets/Scripts/BombSpawn.cs
+++ Assets/Scripts/BombSpawn.cs
@@ -4,32 +4,43 @@ using UnityEngine;
 
 public class BombSpawn : MonoBehaviour
 {
-    [Range(1f, 10f)]
-    public float Distance = 1f;
-    public GameObject BombPrefab;
+    public GameObject BombObj;
+    public GameObject BombContainer;
     public CharacterMovement movementScript;
 
-    private Transform characterTransform;
+    [Range(0, 10)]
+    public int MaxBombs;
     // Start is called before the first frame update
     void Start()
     {
-        characterTransform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Distance = Mathf.Clamp(Distance, 1f, 5f);
         //GameObject obj;
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Vector3 dirVector = Quaternion.Euler(0, movementScript.targetAngle, 0) * Vector3.forward;
+            float heightCompensation = GetComponent<BoxCollider>().size.y * 0.5f - BombObj.GetComponentInChildren<BoxCollider>().size.y * 0.5f;
+            Vector3 v3_heightOffset = new Vector3(0, heightCompensation, 0);
 
-            GameObject bomb = Instantiate(BombPrefab, characterTransform.localPosition + dirVector * Distance, Quaternion.identity) as GameObject;
+            Vector3 position = movementScript.targetPosition - v3_heightOffset;
+            bool available = true;
+            foreach(Transform otherObj in BombContainer.GetComponentInChildren<Transform>())
+            {
+                if (position == otherObj.localPosition)
+                {
+                    available = false;
+                }
+            }
+
+            if(available)
+            {
+                GameObject bomb = Instantiate(BombObj, position, Quaternion.identity, BombContainer.transform) as GameObject;
+            }
+            //bomb.GetComponent<Collider>().enabled = false;
         }
-
-        
 
     }
 }
