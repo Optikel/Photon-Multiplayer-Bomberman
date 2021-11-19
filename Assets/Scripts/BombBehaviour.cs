5b87aa04616a1c9532234dc72e69b9e711dd8dc7
jeryl lah <56944877+OpticalLuck@users.noreply.github.com>
2021-11-19 15:21:56 +0800

    yes

--- Assets/Scripts/BombBehaviour.cs
+++ Assets/Scripts/BombBehaviour.cs
@@ -5,12 +5,18 @@ using UnityEngine.Assertions;
 
 public class BombBehaviour : MonoBehaviour
 {
+    [Header("Prefabs")]
     public GameObject BombMesh;
     public GameObject Explosion;
-    public GameObject[] Indestructables;
+    
+    [Header("Properties")]
     public int Power = 3; //Distance from origin, meaning power 3 is 2 + 1 + 3 = 5 tiles
+    public bool Penetrative = false;
+   
+    [Header("Timing")]
     public float Timer = 3f;
     public float RateOfBlink = 1f;
+
 
     private float CountDown = 0f;
     private float BlinkingTimer = 0f;
@@ -36,28 +42,30 @@ public class BombBehaviour : MonoBehaviour
         switch (m_State)
         {
             case BombState.Blink:
-            {
-                //Hide bomb Mesh
-                if (CountDown >= Timer)
                 {
-                    BombMesh.SetActive(false);
-                    CountDown = 0;
-                    m_State = BombState.Explode;
+                    //Hide bomb Mesh
+                    if (CountDown >= Timer)
+                    {
+                        BombMesh.SetActive(false);
+                        CountDown = 0;
+                        m_State = BombState.Explode;
+                    }
+                    break;
                 }
-                break;
-            }
             case BombState.Explode:
-            {
-                //Destroy Object
-                if (CountDown >= 1)
                 {
-                    CountDown = 0;
-                    Object.Destroy(BombMesh.GetComponentInParent<Transform>().gameObject);
+                    //Destroy Object
+                    if (CountDown >= 1)
+                    {
+                        CountDown = 0;
+                        Object.Destroy(BombMesh.GetComponentInParent<Transform>().gameObject);
+                    }
+                    if (!m_Exploded)
+                    {
+                        CreateExplosion(Explosion, Power);
+                    }
+                    break;
                 }
-                if(!m_Exploded)
-                    CreateExplosion(Explosion, Power);
-                break;
-            }
             default:
                 break;
         }
@@ -68,44 +76,60 @@ public class BombBehaviour : MonoBehaviour
         BlinkingTimer += Time.deltaTime;
 
         Material mat = BombMesh.GetComponent<MeshRenderer>().material;
-        if(BlinkingTimer > intervals * 0.5f)
+        if (BlinkingTimer > intervals * 0.5f)
         {
             mat.SetColor("_Color", mat.GetColor("_Color") == originalColor ? blinkColor : originalColor);
             BlinkingTimer = 0;
         }
     }
 
-    bool CanSpawn(Vector3 Origin, Vector3 Direction, float Magnitude)
-    {
-        //True if no collide, false if collide
-        bool rayCollide = Physics.Raycast(Origin, Direction, Magnitude);
-
-        //return !rayCollide && insideDestructables;
-        return true;    
-    }
-    GameObject InstantiateExplosion(GameObject ExplosionPrefab, Vector3 position, Transform Parent)
-    {
-        Vector3 parentPosition = Parent.position;
-        if(CanSpawn(parentPosition, position.normalized, position.magnitude))
-        {
-            GameObject obj = Instantiate(ExplosionPrefab, Parent);
-            obj.GetComponent<Transform>().localPosition = position;
-        }
-
-        return null;
-    }
     void CreateExplosion(GameObject ExplosionPreFab, float Power = 1)
     {
         m_Exploded = true;
         Transform centerPosition = BombMesh.GetComponent<Transform>().parent;
         //Spawn center
         Instantiate(ExplosionPreFab, centerPosition);
+       
+        InstantiateExplosion(ExplosionPreFab, new Vector3(-1, 0,  0), centerPosition);
+        InstantiateExplosion(ExplosionPreFab, new Vector3( 1, 0,  0), centerPosition);
+        InstantiateExplosion(ExplosionPreFab, new Vector3( 0, 0, -1), centerPosition);
+        InstantiateExplosion(ExplosionPreFab, new Vector3( 0, 0,  1), centerPosition);
+    }
+    void InstantiateExplosion(GameObject ExplosionPrefab, Vector3 direction, Transform Parent)
+    {
+        bool bStop = false;
         for (int i = 1; i < Power; i++)
         {
-            InstantiateExplosion(ExplosionPreFab, new Vector3(-i, 0, 0), centerPosition);           
-            InstantiateExplosion(ExplosionPreFab, new Vector3(i, 0, 0), centerPosition);           
-            InstantiateExplosion(ExplosionPreFab, new Vector3(0, 0, -i), centerPosition);           
-            InstantiateExplosion(ExplosionPreFab, new Vector3(0, 0, i), centerPosition);           
+            if (!bStop)
+            {
+                Vector3 parentPosition = Parent.position;
+                if (CanSpawn(parentPosition, direction.normalized, i))
+                {
+                    GameObject obj = Instantiate(ExplosionPrefab, Parent);
+                    obj.GetComponent<Transform>().localPosition = direction * i;
+
+                    if (!Penetrative)
+                    {
+                        RaycastHit hit;
+                        Ray ray = new Ray(parentPosition, direction.normalized);
+                        if (Physics.Raycast(ray, out hit, i, LayerMask.GetMask("Destructable")))
+                        {
+                            bStop = true;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                break;
+            }
         }
+    }
+
+    bool CanSpawn(Vector3 Origin, Vector3 Direction, float Magnitude)
+    {
+        //True if no collide, false if collide
+        bool rayCollide = Physics.Raycast(Origin, Direction, Magnitude, LayerMask.GetMask("Indestructable"));
+        return !rayCollide;
     }
 }
